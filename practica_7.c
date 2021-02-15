a5517a51253e7b998dbab36c18e4345544bcc0f9
BAR_Hector <BarHector@users.noreply.github.com>
2021-02-15 17:08:46 -0600

    Practica 7 lista

---
+++ practica_7.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+
+int main () { 
+	int numero = 0; 
+	int cuadrado = 0; 
+    int cubo = 0; 
+    int acuCuadrado = 0; 
+    int acuCubo = 0; 
+    int acuNumero = 0; 
+
+    printf ("N%cMERO \tCUADRA \tCUBO\n", 233); 
+
+    numero = 0 + 1; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 2; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 3; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 4;
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 5; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 6; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 7; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 8; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 9; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    numero = 0 + 10; 
+    printf ("%d \t", numero); 
+    cuadrado = numero * numero; 
+    printf ("%d \t", cuadrado); 
+    cubo = numero * numero * numero; 
+    printf ("%d \n", cubo); 
+    acuNumero += numero; 
+    acuCuadrado += cuadrado; 
+    acuCubo += cubo; 
+
+    printf ("La suma de todos los n%cmeros es: %d \n", 163, acuNumero); 
+    printf ("La suma de todos los cuadrados es: %d \n", acuCuadrado); 
+    printf ("La syma de todos los cubos es: %d \n", acuCubo); 
+
+/*
+    for (int i = 1; i <= 10; i++){
+        numero = i; 
+        printf ("%d \t", numero); 
+        cuadrado = numero * numero; 
+        printf ("%d \t", cuadrado); 
+        cubo = numero * numero * numero; 
+        printf ("%d \n", cubo); 
+        acuNumero += numero; 
+        acuCuadrado += cuadrado; 
+        acuCubo += cubo;
+    }
+
+    printf ("La suma de todos los n%cmeros es: %d \n", 163, acuNumero); 
+    printf ("La suma de todos los cuadrados es: %d \n", acuCuadrado); 
+    printf ("La syma de todos los cubos es: %d \n", acuCubo); 
+*/
+
+    return 0; 
+} 
